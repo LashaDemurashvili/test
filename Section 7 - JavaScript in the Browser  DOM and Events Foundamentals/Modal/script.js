b575a37197b6fbcecf07a7999716b3d12f62156d
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-18 17:43:04 +0400

    save 04

--- Section 7 - JavaScript in the Browser  DOM and Events Foundamentals/Modal/script.js
+++ Section 7 - JavaScript in the Browser  DOM and Events Foundamentals/Modal/script.js
@@ -3,36 +3,39 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
-// selected all which has same classes
+// selected all which has same classes -> this is actually array now
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
 // open function
 const openFunction = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 // close function
 const closeFunction = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-//we can  iterate like a Array
-for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', openFunction);
+
+for (let btn of btnsShowModal) {
+    btn.addEventListener('click', openFunction); // without calling function
 }
 
-// close button
-// we don't need to calling the function like this --> closeFunction (); it's automaticaly calling
-btnCloseModal.addEventListener('click', closeFunction);
-overlay.addEventListener('click', closeFunction);
+// create array for close
+let btnsCloseArr = [btnCloseModal, overlay];
+
+// close buttons, iterate over array
+for (let item of btnsCloseArr) {
+    item.addEventListener('click', closeFunction);
+}
 
 // handle key - Esc
 // no matter what we assign to function in this case: function (e) {console.log(e)}
 // it's returns key which we pressed actually
 document.addEventListener('keydown', function (e) {
-  //   console.log(e.key);
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeFunction(); // we need function with execution on this case --> function();
-  }
+    // console.log(e);
+    // console.log(e.key);
+
+    e.key === 'Escape' && !modal.classList.contains('hidden') ? closeFunction() : null;
 });
