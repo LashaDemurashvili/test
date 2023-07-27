4ae6002fe7a2e149aec29b88f6d30b90b90004f0
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-28 00:49:42 +0400

    update training files

--- Section 9 - Data Structures, Modern Operators and Strings/script.js
+++ Section 9 - Data Structures, Modern Operators and Strings/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
     [weekdays[3]]: {
@@ -17,8 +16,56 @@ const openingHours = {
     },
 };
 
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    // ES6 enhanced object literals
+    openingHours,
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(
+            `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+
 // console.log(openingHours);
-for (let day of weekdays){
-    const open = openingHours[day]?.open ?? 'closed' // if we use || (or operator) on sat '0' will be also 'closed', so we use nullish operator on this case.
+for (let day of weekdays) {
+    const open = openingHours[day]?.open ?? 'closed'; // if we use || (or operator) on sat '0' will be also 'closed', so we use nullish operator on this case.
     console.log(`On ${day}, we open at ${open}`);
 }
+
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
+
