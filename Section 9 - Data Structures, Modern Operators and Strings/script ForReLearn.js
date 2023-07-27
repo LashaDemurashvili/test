47230a248c55ae60f86dddc5b791ed1df8bdc085
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-27 23:41:21 +0400

    update training files

--- Section 9 - Data Structures, Modern Operators and Strings/script ForReLearn.js
+++ Section 9 - Data Structures, Modern Operators and Strings/script ForReLearn.js
@@ -232,14 +232,29 @@ console.log(lashaObj);
 */
 
 
-
-
-
+// --------------------------------------------------------------------------------------------------------------------
 /*
-/////////////////////////////////////////////////////////////////////////////// wwwwwwwwwwwwwwwwwwwwww
+/////////////////////////////////////////////////////////////////////////////// // Looping Arrays: The for-of Loop
+const myArray = ['lasha', 'demurashvili', 25, 'Dog']
+for (let item of myArray) {
+    // console.log(item);
+}
 
+///////////////////////////////////////////////////////////////////////////////// entries()
+// console.log(...myArray.entries());
+
+// 1 option
+// for (let item of myArray.entries()){
+//     console.log(`${item[0]+1} - ${item[1]}`);
+// }
+
+// 2 option better  using destruction array method ==> [item, element] of array.entries()
+for (let [i, el] of myArray.entries()){
+    console.log(`${i+1} - ${el}`);
+}
 //////////////////////////////////////////////////////////////////////////////////////////////////// END \
 */
+
 
 
 /*
