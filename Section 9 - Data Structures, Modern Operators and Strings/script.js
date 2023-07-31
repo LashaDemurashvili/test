7a5371af0c1b729585d5ecad0423821ad95563c3
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-31 23:36:48 +0400

    update training files

--- Section 9 - Data Structures, Modern Operators and Strings/script.js
+++ Section 9 - Data Structures, Modern Operators and Strings/script.js
@@ -2,5 +2,13 @@
 
 
 
-const some_string = 'lasha_demurashvili'
-console.log(some_string.slice(3, 10, 1));
+let name = '   laShA'
+
+name = name[0].toUpperCase() + name.slice(1).toLowerCase()
+
+
+name = name.trimStart()
+console.log(name.length);
+console.log(name);
+
+
