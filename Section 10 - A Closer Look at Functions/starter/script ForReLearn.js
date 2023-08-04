bce88d30787ebb68e638b9eb262d724fdb94dc70
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-04 23:31:49 +0400

    updated files

--- Section 10 - A Closer Look at Functions/starter/script ForReLearn.js
+++ Section 10 - A Closer Look at Functions/starter/script ForReLearn.js
@@ -26,7 +26,7 @@ const createBooking = function (
     bookings.push(booking); // push to the array
 };
 
-createBooking()                                                 // not give parameters, then using default parameters
+createBooking()                       // not give parameters, then using default parameters
 createBooking(1,1,1)                // give new parameters
 createBooking(1, undefined, 2)    //undefined -> means same as we don't give parameter, so if there is default parameter then call it
 
@@ -40,12 +40,104 @@ console.log(bookings)  // log array
 // ------------------------------------------------------------------------------------------------------------ NEW LINE
 
 /*
-/////////////////////////////////////////////////////////////////////////////// wwwwwwwwwwwwwwwwwwwwww_CODE_TITLE
+///////////////////////////////////////////////////////////////////////////////// How Passing Arguments Works: Values vs. Reference
+const flight = 'BOEING777';
+const lasha = {
+    name: 'Lasha Demurashvili',
+    passport: 284007,
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 284007) {
+        alert('Checked in');
+    } else {
+        alert('Wrong passport!');
+    }
+};
+
+checkIn(flight, lasha);
+
+// after function invoke passenger.name in lasha object will be changed
+console.log(flight);
+console.log(lasha);
+
+const changePassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 9999999999)
+}
+// after that lasha has new passport number
+changePassport(lasha)
+console.log(`New passport is ${lasha.passport}`)
+
+// and if we check, see that new password don't match
+checkIn(flight, lasha)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////// END \
+*/
+
+
+// ------------------------------------------------------------------------------------------------------------ NEW LINE
+
+/*
+/////////////////////////////////////////////////////////////////////////////// counter
+const counter = {
+    value: 22,
+    inc() {
+        this.value++;
+    }
+}
+
+// log first declared value
+console.log(counter.value)
+
+// update original value
+for (let i = 0; i < 15; i++) {
+    counter.inc()
+}
+
+// log changed value
+console.log(typeof counter.value)
+console.log(counter.value)
+
+// so after we call function, in the original object change value to new one
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////// END \
+*/
+
+
+// ------------------------------------------------------------------------------------------------------------ NEW LINE
+
+/*
+/////////////////////////////////////////////////////////////////////////////// Functions Accepting Callback Functions
 CODE_HERE
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////// END \
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // template
