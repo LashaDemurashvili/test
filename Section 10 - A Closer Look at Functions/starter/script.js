aa7ec8101b97cd95c9e5c057753835f595283897
Lasha Demurashvili <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-05 03:19:01 +0400

    updated

--- Section 10 - A Closer Look at Functions/starter/script.js
+++ Section 10 - A Closer Look at Functions/starter/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-'tsting'
+// The call and apply Methods
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    plane: 'Boeing-',
+    bookings: [],
+    // book function(){}
+    book(flightNum, name, plane) {
+        console.log(`${name} - booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}, to the ${this.plane} ${plane} !`);
+    }
+};
+
+
+lufthansa.book(239, 'Lasha Demurashvili', '777')
+
