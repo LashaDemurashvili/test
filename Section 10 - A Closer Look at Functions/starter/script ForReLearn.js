83ba49f8dd0be1b9045cde651112bac1722c4596
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-04 21:40:35 +0400

    updated files

--- Section 10 - A Closer Look at Functions/starter/script ForReLearn.js
+++ Section 10 - A Closer Look at Functions/starter/script ForReLearn.js
@@ -1,18 +1,41 @@
 'use strict';
 
 
-
-
-
 /*
-/////////////////////////////////////////////////////////////////////////////// wwwwwwwwwwwwwwwwwwwwww_CODE_TITLE
-CODE_HERE
+/////////////////////////////////////////////////////////////////////////////// default parameters
+const bookings = [];
+
+const createBooking = function (
+    flightNum = 11,
+    numPassengers = flightNum * 22,
+    price = 33) {
+    // es5 method for default parameters
+
+    // flightNum = flightNum || 10;     // this two is the same
+    // flightNum ||= 10;                   // this two is the same
+    // numPassengers ||= 15;
+    // price ||= 20;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking()                     // not give parameters, then using default parameters
+createBooking(1,1,1)                // give new parameters
+createBooking(1, undefined, 2)    //undefined -> means same as we don't give parameter, so if there is default parameter then call it
+
+
+console.log(bookings)  // log array
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////// END \
 */
-
-
 
 
 // ------------------------------------------------------------------------------------------------------------ NEW LINE
@@ -24,20 +47,6 @@ CODE_HERE
 
 //////////////////////////////////////////////////////////////////////////////////////////////////// END \
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // template
--- Section 10 - A Closer Look at Functions/starter/script.js
+++ Section 10 - A Closer Look at Functions/starter/script.js
@@ -1,11 +1,18 @@
 'use strict';
 
 
-///////////////////////////////////////
-// Default Parameters
 const bookings = [];
 
-const createBooking = function (flightNum,numPassengers,price) {
+const createBooking = function (
+    flightNum = 11,
+    numPassengers = flightNum * 22,
+    price = 33) {
+    // es5 method for default parameters, OR assignment
+
+    // flightNum = flightNum || 10;     // this two is the same
+    // flightNum ||= 10;                   // this two is the same
+    // numPassengers ||= 15;
+    // price ||= 20;
 
     const booking = {
         flightNum,
@@ -14,9 +21,12 @@ const createBooking = function (flightNum,numPassengers,price) {
     };
 
     console.log(booking);
-    bookings.push(booking);
+    bookings.push(booking); // push to the array
 };
 
+createBooking()                                                 // not give parameters, then using default parameters
+createBooking(1,1,1)                // give new parameters
+createBooking(1, undefined, 2)    //undefined -> means same as we don't give parameter, so if there is default parameter then call it
 
-createBooking()
-console.log(bookings)
+
+console.log(bookings)  // log array
