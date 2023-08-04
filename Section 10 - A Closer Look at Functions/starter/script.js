0bc3c168c1050dc8a741c3432261a1e5e16763c3
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-04 21:07:15 +0400

    update

--- Section 10 - A Closer Look at Functions/starter/script.js
+++ Section 10 - A Closer Look at Functions/starter/script.js
@@ -1,27 +1,22 @@
 'use strict';
 
 
-
-
-
 ///////////////////////////////////////
 // Default Parameters
 const bookings = [];
 
-const createBooking = function (
-    flightNum,
-    numPassengers = 1,
-    price = 199 * numPassengers
-) {
-    // ES5
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
+const createBooking = function (flightNum,numPassengers,price) {
 
     const booking = {
         flightNum,
         numPassengers,
         price,
     };
+
     console.log(booking);
     bookings.push(booking);
 };
+
+
+createBooking()
+console.log(bookings)
