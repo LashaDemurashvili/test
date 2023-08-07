fc0e7391551bd4700879c11edcbc107c550e1e58
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-08 01:03:50 +0400

    updated files

--- Section 10 - A Closer Look at Functions/starter/script.js
+++ Section 10 - A Closer Look at Functions/starter/script.js
@@ -1,4 +1,25 @@
 'use strict';
 
 
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+    let passengerCount = 0;
+
+    return function () {
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+booker();
+
+console.dir(secureBooking);
+
+
 
