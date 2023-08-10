b399a63e58c0b8a0a268f4f704a18b91660385b5
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-11 00:07:52 +0400

    update files

--- Section 11 - Working With Arrays - Bankist/starter/recap/script.js
+++ Section 11 - Working With Arrays - Bankist/starter/recap/script.js
@@ -1,18 +1,7 @@
 
 /*
-/////////////////////////////////////////////////////////////////////////////// wwwwwwwwwwwwwwwwwwwwww_CODE_TITLE
-CODE_HERE
+///////////////////////////////////////////////////////////////////////////////// Simple Array Methods
 
-
-//////////////////////////////////////////////////////////////////////////////////////////////////// END \
-*/
-
-
-// ------------------------------------------------------------------------------------------------------------ NEW LINE
-
-
-/////////////////////////////////////////////////
-// Simple Array Methods
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log('slice');
@@ -22,37 +11,77 @@ console.log(arr.slice(2, 4));
 console.log(arr.slice(-2));
 console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
+
+// shallow copy
 console.log(arr.slice());
 console.log([...arr]);
+const arrCopy = [...arr]
 
-
+// splice - delete from original
 console.log('\nsplice');
 // SPLICE
-arr.splice(-1);
+arr.splice(-1); // deleted -1 position
 console.log(arr);
 arr.splice(1, 2);
 console.log(arr);
+console.log(arrCopy.splice(0, 3)); // log but, delete from original also,
+console.log(`Array after log splice - ${arrCopy}`);
 
 
 console.log('\nreverse');
 // REVERSE
 arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
+// remain original one after log
+console.log([...arr2].reverse(), 'reverse'); // original arr2 doesn't modify
+console.log(arr2, 'original');
+
+console.log(arr2.reverse());    // original arr2 does modify now, after log also
 console.log(arr2);
 
 
-console.log('\nconcat');
 
+console.log('\nconcat');
 // CONCAT
 const letters = arr.concat(arr2);
 console.log(letters);
-console.log([...arr, ...arr2]);
+console.log([...arr, ...arr2]);  // using spread operator
 
 
 console.log('\njoin');
 // JOIN
 console.log(letters.join(' - '));
+console.log(letters);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////// END \
+*/
+
+
+// ------------------------------------------------------------------------------------------------------------ NEW LINE
+
+
+/*
+/////////////////////////////////////////////////////////////////////////////// wwwwwwwwwwwwwwwwwwwwww_CODE_TITLE
+CODE_HERE
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////// END \
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
