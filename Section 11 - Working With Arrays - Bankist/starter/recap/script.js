a46f3da75799749acfd186e3c67fd7472015dac5
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-12 01:17:20 +0400

    update files

--- Section 11 - Working With Arrays - Bankist/starter/recap/script.js
+++ Section 11 - Working With Arrays - Bankist/starter/recap/script.js
@@ -132,16 +132,36 @@ movements.forEach(function (mov, i, arr) {
 */
 
 
+
+// ------------------------------------------------------------------------------------------------------------ NEW LINE
+
+
+/*
+/////////////////////////////////////////////////////////////////////////////// // forEach With Maps and Sets
+// Map
 const currencies = new Map([
     ['USD', 'United States dollar'],
     ['EUR', 'Euro'],
     ['GBP', 'Pound sterling'],
 ]);
 
+currencies.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`);
+});
 
-currencies.forEach(function (x,y,z) {
-    console.log(`${x} \n${y} \n${z}`);
-})
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`);
+    // console.log(_);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////// END \
+*/
+
+
+
 
 
 
