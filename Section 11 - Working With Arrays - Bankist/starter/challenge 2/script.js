c63e02de4f6db4a5c1348f6b4adbf343ff590f29
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-08-28 22:57:22 +0400

    update files

--- Section 11 - Working With Arrays - Bankist/starter/challenge 2/script.js
+++ Section 11 - Working With Arrays - Bankist/starter/challenge 2/script.js
@@ -20,5 +20,46 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
+// const calcAverageHumanAge = function(dogsAgesArray){
+//     const x = dogsAgesArray.map(function(x) {
+//         if(x <= 2) {
+//             return x * 2
+//         } else if (x > 2){
+//             return 16 + x * 4
+//         }
+//     })
+//     const y = x.filter(x => x >= 18)
+//     const aver = y.reduce((acc, curr) => acc+ curr)
+//     console.log(aver/(y.length));
+// }
+//
+//
+// const data1 = [5, 2, 4, 1, 15, 8, 3]
+// const data2 = [16, 6, 10, 5, 6, 1, 4]
+//
+// calcAverageHumanAge(data1)
+// calcAverageHumanAge(data2)
+
+const calcAverageHumanAge = function (ages) {
+    const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+    const adults = humanAges.filter(age => age >= 18);
+    // console.log(humanAges);
+    // console.log(adults);
+
+    // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+    const average = adults.reduce(
+        (acc, age, i, arr) => acc + age / arr.length,
+        0
+    );
+
+    // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
+    return average;
+};
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
 
 
