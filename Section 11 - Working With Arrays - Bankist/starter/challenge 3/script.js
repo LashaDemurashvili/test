76c2e60e991e97d999a2070292298d1777c7670a
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-09-03 22:09:35 +0400

    update files

--- Section 11 - Working With Arrays - Bankist/starter/challenge 3/script.js
+++ Section 11 - Working With Arrays - Bankist/starter/challenge 3/script.js
@@ -8,16 +8,15 @@ continue of Coding Challenge #2
 just using chaining methods
 */
 
-const calcAverageHumanAge = ages => ages
-    .map(age => (age <= 2 ? age * 2 : age * 4 + 16))
-    .filter(age => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-
-
-const data1 = [5, 2, 4, 1, 15, 8, 3];
-const data2 = [16, 6, 10, 5, 6, 1, 4];
-const avg1 = calcAverageHumanAge(data1);
-const avg2 = calcAverageHumanAge(data2);
-console.log(avg1, avg2);
-
+// const calcAverageHumanAge = ages => ages
+//     .map(age => (age <= 2 ? age * 2 : age * 4 + 16))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+//
+//
+// const data1 = [5, 2, 4, 1, 15, 8, 3];
+// const data2 = [16, 6, 10, 5, 6, 1, 4];
+// const avg1 = calcAverageHumanAge(data1);
+// const avg2 = calcAverageHumanAge(data2);
+// console.log(avg1, avg2);
 
